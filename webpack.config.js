78b15f4b19495e1f5e755d5f1a523d3542be7947
Brendan Holmes <brendholmes@gmail.com>
2020-04-17 08:02:41 +1000

    rename node server entry point and update links

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,25 @@
 // Do not bundle node_modules dependencies
 var nodeExternals = require('webpack-node-externals');
+const path = require('path');
 
 module.exports = {
+  entry: './server.js',
+  output: {
+    // options related to how webpack emits results
+    path: path.resolve(__dirname, "dist"), // string
+    // the target directory for all output files
+    // must be an absolute path (use the Node.js path module)
+    filename: "main.js", // string
+    // the filename template for entry chunks
+    //publicPath: "/assets/", // string
+    // the url to the output directory resolved relative to the HTML page
+    //library: "MyLibrary", // string,
+    // the name of the exported library
+    //libraryTarget: "umd", // universal module definition
+    // the type of the exported library
+    /* Advanced output configuration (click to show) */
+    /* Expert output configuration (on own risk) */
+  },
   target: 'node',
   externals: [nodeExternals()],
   module: {
